58bd24e4aa2d2c477bdbb4ddfb8e5e590812ebb2
Igor Pavetka <uropb.86@gmail.com>
2022-07-26 01:39:08 +0300

    feat: 07-conditions-n-loops-tasks.js - partly done (3)

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -166,8 +166,11 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const { center, radius } = circle;
+  const { x, y } = center;
+  return ((point.x - x) ** 2 + (point.y - y) ** 2) < radius ** 2;
+  // throw new Error('Not implemented');
 }
 
 
